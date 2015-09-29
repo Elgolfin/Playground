5d218330869a1255708742c516c79728bd5945e1
unknown <nicolas.rose@W398.orckestra.local>
2015-09-28 21:20:28 -0400

    Initial commit (Grid Class, Associated Unit Tests and some methods to calculate the nearest free point in the grid)

---
+++ BackToTheCode/BackToTheCode/Grid.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackToTheCode
+{
+    public class Grid
+    {
+        public int cols = 35;
+        public int rows = 20;
+        string[] lines = new string[20];
+        string[,] grid; // . = neutral; 0 = me; 1, 2 or 3 = opponent id
+        public int myPositionX;
+        public int myPositionY;
+
+        public Grid()
+        {
+            grid = new string[rows, cols];
+        }
+
+        public void Initialize(string line, int lineNumber)
+        {
+            int cptCol = 0;
+            foreach (char c in line)
+            {
+                grid[lineNumber, cptCol++] = c.ToString();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < rows; i++)
+            {
+                StringBuilder line = new StringBuilder();
+                for (int j = 0; j < cols; j++)
+                {
+                    line.Append(grid[i,j]);
+                }
+                str.AppendLine(line.ToString());
+                
+            }
+
+            return str.ToString();
+        }
+
+        public static double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            double res = Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+            return res;
+        }
+
+        public string GetTheNearestNeutralPoint()
+        {
+            double refDistance = 999;
+            int refX = 0;
+            int refY = 0;
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    if (grid[y,x] != ".") { continue; }
+                    double distance = GetDistance(myPositionX, myPositionY, x, y);
+                    if (refDistance > distance) {
+                        refDistance = distance;
+                        refX = x;
+                        refY = y;
+                    }
+                    if (refDistance == 1) { break; }
+                }
+                if (refDistance == 1) { break; }
+            }
+            return String.Format("{0} {1}", refX.ToString(), refY.ToString());
+        }
+    }
+}
---
+++ BackToTheCode/BackToTheCodeUnitTests/UnitTest1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackToTheCode;
+
+namespace BackToTheCodeUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGrid()
+        {
+            Grid myGrid = new Grid();
+
+            string testStr =   @"...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................
+...................................";
+
+            for (int i = 0; i < myGrid.rows; i++)
+            {
+                myGrid.Initialize("...................................", i);
+            }
+
+            Assert.AreEqual(testStr.Trim(), myGrid.ToString().Trim());
+        }
+
+        [TestMethod]
+        public void DistanceBetweenTwoPoints()
+        {
+            Assert.AreEqual(1, Grid.GetDistance(0, 0, 0, 1));
+            Assert.AreEqual(2, Grid.GetDistance(0, 0, 0, 2));
+            Assert.AreEqual(53, Grid.GetDistance(0, 0, 34, 19));
+        }
+
+        [TestMethod]
+        public void GetNearestNeutralePoint1()
+        {
+            Grid myGrid = new Grid();
+            myGrid.myPositionX = 0;
+            myGrid.myPositionY = 0;
+
+            myGrid.Initialize("0..................................", 0);
+            myGrid.Initialize("...................................", 1);
+            myGrid.Initialize("...................................", 2);
+            myGrid.Initialize("...................................", 3);
+            myGrid.Initialize("...................................", 4);
+            myGrid.Initialize("...................................", 5);
+            myGrid.Initialize("...................................", 6);
+            myGrid.Initialize("...................................", 7);
+            myGrid.Initialize("...................................", 8);
+            myGrid.Initialize("...................................", 9);
+            myGrid.Initialize("...................................", 10);
+            myGrid.Initialize("...................................", 11);
+            myGrid.Initialize("...................................", 12);
+            myGrid.Initialize("...................................", 13);
+            myGrid.Initialize("...................................", 14);
+            myGrid.Initialize("...................................", 15);
+            myGrid.Initialize("...................................", 16);
+            myGrid.Initialize("...................................", 17);
+            myGrid.Initialize("...................................", 18);
+            myGrid.Initialize("...................................", 19);
+
+            Assert.AreEqual("1 0", myGrid.GetTheNearestNeutralPoint());
+
+        }
+
+        [TestMethod]
+        public void GetNearestNeutralePoint2()
+        {
+            Grid myGrid = new Grid();
+            myGrid.myPositionX = 0;
+            myGrid.myPositionY = 0;
+
+            myGrid.Initialize("00000000000000000000000000000000000", 0);
+            myGrid.Initialize("00000000000000000000000000000000000", 1);
+            myGrid.Initialize("00000000000000000000000000000000000", 2);
+            myGrid.Initialize("00000000000000000000000000000000000", 3);
+            myGrid.Initialize("00000000000000000000000000000000000", 4);
+            myGrid.Initialize("00000000000000000000000000000000000", 5);
+            myGrid.Initialize("00000000000000000000000000000000000", 6);
+            myGrid.Initialize("00000000000000000000000000000000000", 7);
+            myGrid.Initialize("00000000000000000000000000000000000", 8);
+            myGrid.Initialize("00000000000000000000000000000000000", 9);
+            myGrid.Initialize("00000000000000000000000000000000000", 10);
+            myGrid.Initialize("00000000000000000000000000000000000", 11);
+            myGrid.Initialize("00000000000000000000000000000000000", 12);
+            myGrid.Initialize("00000000000000000000000000000000000", 13);
+            myGrid.Initialize("00000000000000000000000000000000000", 14);
+            myGrid.Initialize("00000000000000000000000000000000000", 15);
+            myGrid.Initialize("00000000000000000000000000000000000", 16);
+            myGrid.Initialize("00000000000000000000000000000000000", 17);
+            myGrid.Initialize("00000000000000000000000000000000000", 18);
+            myGrid.Initialize("0000000000000000000000000000000000.", 19);
+
+            Assert.AreEqual("34 19", myGrid.GetTheNearestNeutralPoint());
+
+        }
+    }
+}

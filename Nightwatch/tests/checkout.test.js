caa88da5065f9c2e586fde1da11df07137d18a38
Elgolfin <elgo@nicomputer.net>
2016-03-26 20:32:13 -0400

    Did some more tests

--- Nightwatch/tests/checkout.test.js
+++ Nightwatch/tests/checkout.test.js
@@ -14,16 +14,30 @@ module.exports = {
     checkout.navigate()
 		.waitForElementPresent('body', 1000);
     
+    browser.expect.element('[data-qa=minicart-count]').text.equal("");
+    
     search.submit(search.variables().sortKeywordTerm)
         .click('@searchProductName');
     
     // /*
     product.firstAddToCart()
         .waitForElementPresent('[data-qa=minicart-count]', 2000)
-        .assert.containsText('[data-qa=minicart-count]', 1)
-        .addToCart()
-        .assert.containsText('[data-qa=minicart-count]', 2);
+        .addToCart();
+    
+    //
     // */
+    
+    browser.expect.element('[data-qa=minicart-count]').text.equal(2);
+    
+    let result = browser.getText('[data-qa=minicart-count]', function(result){
+        console.log(result.value);
+        browser.expect.element('[data-qa=minicart-count]').to.be.visible;
+        let regex = new RegExp("^"+ result.value +"$");
+        browser.expect.element('[data-qa=minicart-count]').text.matches(regex);
+        browser.expect.element('[data-qa=minicart-count]').text.equal(result.value);
+    });
+    
+    
     browser.end();
   }
 };

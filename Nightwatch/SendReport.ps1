094a5ef389b243514dc52f8e327037b6144f702a
Elgolfin <elgo@nicomputer.net>
2016-03-28 20:43:45 -0400

    Add a script and the related task scheduler to run the nightwatch.js test every 5 minutes.

---
+++ Nightwatch/SendReport.ps1
@@ -0,0 +1,11 @@
+﻿[cmdletbinding()]
+param([string[]]$To)
+
+
+cd C:\Users\me\Documents\Repositories\Playground\Nightwatch\
+try {
+	node nightwatch.js -e chrome
+} catch {
+	
+}
+Send-MailMessage -From me@yopmail.com -To $to -Attachments .\reports\CHROME_49.0.2623.87_XP_checkout.test.xml -Subject 'Nightwatch.js Results' -Body 'See attachments.' -SmtpServer 'smtp.mydomain.local'
